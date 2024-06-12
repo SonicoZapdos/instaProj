fb2bde37935ec876bbd36169c8d053bc83829e5c
RafaelOBrasil <rafael-lunia@hotmail.com>
2024-06-11 21:32:40 -0300

    Validações

--- instaProj/Models/User.cs
+++ instaProj/Models/User.cs
@@ -1,12 +1,26 @@
-﻿namespace instaProj.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace instaProj.Models
 {
     public class User
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-        public string? Telefone { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "O campo Telefone é obrigatório.")]
+        [Phone(ErrorMessage = "O campo Telefone deve ser um número de telefone válido.")]
+        public string Telefone { get; set; }
+
         public string? Url { get; set; }
         public string? Bio { get; set; }
         public string? PictureLocal {  get; set; }
